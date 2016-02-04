52061467383043616de3c8fbf4d870c701c7aa70
Waldo J. Saccaco <saccaco@live.conm>
2016-02-04 00:41:09 +0000

    agregando ruta index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'pages/login'
   
-  root 'home#index'
+  root 'pages#login'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
