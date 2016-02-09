06644195a0bd0d2cbf428332a13a833f1512899e
Waldo J. Saccaco <saccaco@live.conm>
2016-02-09 00:17:25 +0000

    agregnado llamar un taxi

---
+++ app/assets/javascripts/maps.js
@@ -0,0 +1,23 @@
+
+$( document ).ready(function() {
+    var map = L.map('map').setView([-12.09081,  -77.02274], 15);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets-basic',
+        accessToken: 'pk.eyJ1IjoiY2FtZWYiLCJhIjoiYjQ3NGI0YTE5NGI3OWYyNGZhMzFlMDI1NDE3MTI1NzAifQ.TuW75_HLjFL4k-qGZ_2CLg'
+    }).addTo(map);
+
+    var marker = L.marker([-12.09081,  -77.02274], {
+        draggable : true
+    });
+        marker.addTo(map)
+            .bindPopup('waldo');
+        marker.on('click', function ( event ) {
+            this.setPopupContent(event.latlng.toString())
+            console.log( event.latlng.toString() )
+        })
+
+
+});
