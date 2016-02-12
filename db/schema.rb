0b594523f4e329c1a289f560ba821b970d51d5ff
Waldo J. Saccaco <w.saccaco@gmail.com>
2016-02-12 19:40:23 +0000

    add label to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204020557) do
+ActiveRecord::Schema.define(version: 20160211031128) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "first_name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160204020557) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "role"
+    t.string   "user_type"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
