7bee58e97fd634dc25566203501e0c3c19683199
gisslescano <gisslescano@gmail.com>
2016-02-18 03:17:22 +0000

    haciendo que funcione el login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+ devise_for :users
+  
   resources :profiles
+  
+
+  
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  
   get 'pages/index'
 
 
